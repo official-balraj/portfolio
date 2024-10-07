a7a1f93f5303bcc565d30509b339d22d322fc895
Balraj <balraj@192.168.1.7>
2024-10-07 22:55:09 +0530

    Setup the data object

---
+++ src/data/profileData.ts
@@ -0,0 +1,111 @@
+const profileData = {
+  personalDetails: {
+    name: "Balraj Singh",
+    nickname: "Balraj",
+    email: "official.balraj@gmail.com",
+    nationality: "Indian",
+    location: "Mohali, Punjab, India",
+    education: "Bachelors in Computer Science & Engineering",
+    hobby: "Learning, Gardening",
+    interests: "Travelling, Design, Reading Book",
+  },
+  summary:
+    "Technical Engineering Manager with 10+ years of experience in development and leadership across HCM Suite, SaaS, SPA, Mobile, and Responsive UI/UX. Leverages technical and leadership skills to foster innovation, quality, and efficiency, creating high-performance teams.",
+  skills: {
+    technical: [
+      "React",
+      "TypeScript",
+      "Material UI",
+      "Storybook",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap",
+      "Rest API",
+      "Spring Boot",
+      "Java",
+      "UI/UX",
+      "SPA",
+      "PWA",
+      "Responsive Design",
+      "Figma",
+      "Azure",
+      "Micro-frontend",
+      "Microservices",
+    ],
+    management: [
+      "Team leadership & management",
+      "Project & People management",
+      "Proactive communication",
+      "Short-term & long-term planning",
+      "Stakeholder communication",
+      "Process improvement",
+    ],
+  },
+  experience: [
+    {
+      role: "Engineering Manager (Technical)",
+      company: "Catalystone Solutions",
+      duration: "Nov 2019 - Present",
+      responsibilities: [
+        "Lead 3 engineering teams & 4 modules towards successful delivery.",
+        "Migrated module from monolith to React Micro-frontend.",
+        "Hands-on involvement in Sprints to pace up team deliverables.",
+        "Communicate with stakeholders to manage expectations.",
+        "Monitor team adherence to expectations and address deviations.",
+        "Improve processes, practices, and tools to eliminate waste and enhance delivery speed.",
+        "Define team responsibilities and technical roadmap.",
+        "Provide recognition and constructive feedback to team members.",
+      ],
+      products: ["Learning Systems", "Employee Data Import", "HR Overview"],
+    },
+    {
+      role: "Team Lead & Front-end Developer",
+      company: "Catalystone Solutions",
+      duration: "Nov 2016 - Nov 2019",
+      responsibilities: [
+        "Led a project and a team of 15+ developers.",
+        "Developed Design System framework and revamped the complete UI.",
+        "Short-term & long-term planning, keeping stakeholders updated.",
+        "Deliver high-quality releases on time and gain technical expertise.",
+        "Perform peer code reviews and add/edit code for new modules and changes.",
+      ],
+      products: ["Design System", "Front-end Library", "Revamped Product UI"],
+    },
+    {
+      role: "Full Stack Developer",
+      company: "Catalystone Solutions",
+      duration: "May 2014 - Nov 2016",
+      responsibilities: [
+        "Led the front-end development team.",
+        "Implemented best practices for UI/UX and responsive design.",
+        "Collaborated with cross-functional teams to ensure project success.",
+      ],
+      products: ["Performance Management", "Business Planner"],
+    },
+  ],
+  education: [
+    {
+      degree: "B.Tech - Computer Science & Engineering",
+      institution: "Punjab Technical University",
+      graduationYear: 2014,
+    },
+  ],
+  certifications: [
+    {
+      name: "Professional Scrum Master - 1",
+      year: 2019,
+      certUrl: "https://www.scrum.org/certificates/467546",
+    },
+    {
+      name: "Meta Front-end Developer",
+      certUrl:
+        "https://www.coursera.org/account/accomplishments/professional-cert/XX5L9WL28MBV",
+    },
+  ],
+  extracurriculars: [
+    "Graphic Designing",
+    "Research & Learning New Technologies",
+  ],
+  dateOfBirth: "11.08.1992",
+};
